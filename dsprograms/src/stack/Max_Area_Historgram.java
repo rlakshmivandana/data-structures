418e5f36de22aa691869a7efae381ebb9239f8a6
vandana gandluri <vandana gandluri>
2022-11-19 07:53:45 +0530

    added programs

---
+++ dsprograms/src/stack/Max_Area_Historgram.java
@@ -0,0 +1,30 @@
+package stack;
+
+import java.util.Stack;
+
+public class Max_Area_Historgram {
+	public static void main(String[] args) {
+		int arr[] = { 3, 4, 7, 2, 4, 5 };
+		maxArea(arr, arr.length);
+	}
+
+	private static void maxArea(int[] a, int length) {
+		Stack<Integer> s = new Stack<Integer>();
+		int i = 0, max = 0, currMax, area, maxArea = 0;
+		while (i < length) {
+			if (s.empty() || a[s.peek()] <= a[i]) {
+				s.push(i);
+				i++;
+			} else {
+				currMax = s.pop();
+				area = a[currMax] * (s.empty() ? i : i - s.peek()-1);
+				if (area > maxArea) {
+					maxArea = area;
+				}
+			}
+
+		}
+		System.out.println("max area"+ maxArea);
+	}
+
+}
